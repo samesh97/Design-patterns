f681bfa774485dd258df64209615db7cf4df115b
samesh97 <Alahakoon123@>
2021-09-30 17:49:49 +0530

    Factory design pattern was updated.

--- Factory/src/Main.ts
+++ Factory/src/Main.ts
@@ -5,5 +5,6 @@ export class Main
     public static main() : void
     {
         var bike = VehicleFactory.getInstance("Bike",110);
+        var car = VehicleFactory.getInstance("Car",1500);
     }
 }
